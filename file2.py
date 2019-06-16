4127a0f9e7ea9fc03ba06e0e1a122047f0c1e69b
Yash Jain <yash.jain106@gmail.com>
2019-06-17 00:34:45 +0530

    16 jun 2019

--- file2.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 12 11:30:05 2019 @author: JAINS
-"Welcome to Python"
-x=5
-x
---
+++ test run 01.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+list=[1,2,3,4]
+list
+
