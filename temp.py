2f2d105aa61a8132e539361fe5d4ccc871be4e6e
Yash Jain <yash.jain106@gmail.com>
2019-06-30 12:22:33 +0530

    30 Jul 2019

--- temp.py
+++ temp.py
@@ -5,7 +5,8 @@ Spyder Editor
 This is a temporary script file
 """
 from pydataset import data #importing dataset
-import numpy; import pandas
+import numpy
+import pandas as pd
 mt_cars=data('mtcars') #defining the name to another dataset
 mt_cars
 
@@ -16,5 +17,13 @@ mt_cars.shape[1] #no of colums
 columns=mt_cars.columns.astype('category')
 columns
 
-len(dir(pandas))
+len(dir(pd))
 len(dir(numpy))
+
+pd2=pd.read_clipboard()
+pd2
+pd2.to_clipboard()
+pd2[1:3]
+pd2.iloc[1:9,3:6]
+pd2.loc[1:4,['OrderDate','Region']]
+pd2
