bd48f014855307e70708272871f64194999b0b08
Yash Jain <yash.jain106@gmail.com>
2019-07-20 17:19:28 +0530

    20 Jul 2019 - Draft 2

--- Project 1.py
+++ Project 1.py
@@ -217,7 +217,6 @@ plt.show()
 
 #6. Door Number 
  
-
 plt.figure(figsize=(25,10))
 
 #plot 1
@@ -408,8 +407,6 @@ def scatterplot(df,var):
 
 #5. Creating a Categorical Variable - Car Class
     
-    price=cars_data['price'].tolist()
-    price
     carsrange=[]
     for i in cars_data['price']:
         if (i>0 and i<9000): carsrange.append('Low')
@@ -456,16 +453,17 @@ sns.pairplot(cars_data, x_vars=['boreratio','compressionratio'], y_vars='price',
     plt.show()
     plt.tight_layout()    
 
-#Correlation with price(target variable)
+#Correlation with price(target variable) for numeric data
     
 corr=cars_data.corr().round(3).loc['price']
 corr=pd.DataFrame(corr)
 corr
 result=[]
-    for i in corr['price']:
-        if (i>-1 and i<-0.4): result.append('strong negative ')
-        elif (i>-0.4 and i<-0.2): result.append('moderate negative ')
-        elif (i>-0.2 and i<0): result.append('weak negative ')
+    
+for i in corr['price']:
+        if (i>-1 and i<-0.4): result.append('strong negative')
+        elif (i>-0.4 and i<-0.2): result.append('moderate negative')
+        elif (i>-0.2 and i<0): result.append('weak negative')
         elif(i>0 and i<0.2): result.append('weak positive')
         elif(i>0.2 and i<0.5): result.append('moderate positive')
         else : result.append('strong positive')
@@ -479,5 +477,238 @@ labels=corr['correlation'].unique()
 plt15 = corr['correlation'].value_counts().tolist()
 plt.pie(plt15, labels=plt15, autopct='%1.1f%%')
 plt.legend(labels, loc=1)
+
+corr.loc[:,'correlation']
+
+"""
+Variables to be taken for analysis (Based on Visualization) :
+    
+- Car Range
+- Engine Type 
+- Fuel type 
+- Car Body 
+- Aspiration 
+- Cylinder Number
+- Car Length
+- Car Width 
+- Drivewheel 
+- Curbweight 
+- Car Volume
+- Engine Size 
+- Boreratio 
+- Horse Power 
+- Wheel base 
+- Fuel Economy
+
+"""
+
+#Regression
+
+cars=cars_data[['price','carsrange','enginetype','fueltype','carbody','aspiration','cylindernumber','carlength','carwidth','drivewheel','curbweight','carvolume','enginesize','boreratio','horsepower','wheelbase','fueleconomy']]
+cars.head()
+
+sns.pairplot(cars)
+plt.show()
+
+#Dummy Variables
+
+def dummies(x,df):
+    var=pd.get_dummies(df[x], drop_first=True)
+    df=pd.concat([df,var], axis=1)
+    df.drop([x], axis=1, inplace=True)
+    return df
+
+cars = dummies('fueltype',cars)
+cars = dummies('aspiration',cars)
+cars = dummies('carbody',cars)
+cars = dummies('drivewheel',cars)
+cars = dummies('enginetype',cars)
+cars = dummies('cylindernumber',cars)
+cars = dummies('carsrange',cars)
+
+cars.head()
+cars.shape
+
+
+#Train-Test Split and Feature Scaling
+
+from sklearn.model_selection import train_test_split
+np.random.seed(0)
+df_train, df_test=train_test_split(cars, train_size=0.6, test_size=0.4, random_state=100)
+
+df_train.head() #training set
+df_test.head() #test set
+
+from sklearn.preprocessing import MinMaxScaler #feature scaling 
+scaler=MinMaxScaler()
+
+high_corr=df_train.corr().loc[df_train.corr()['price']>0.75]['price']  #highly correlated values with price
+high=high_corr.index.drop('price').tolist()
+
+low_corr=df_train.corr().loc[df_train.corr()['price']<-0.45]['price']
+low=low_corr.index.tolist()
+
+num_vars=high+low
+num_vars
+df_train[num_vars] = scaler.fit_transform(df_train[num_vars])
+
+df_train.head()
+df_train.describe().round(2)
+
+#splitting into x and y 
+y_train=df_train.pop('price')
+x_train=df_train
+
+
+#4. Model Building
+
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LinearRegression
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.ensemble import RandomForestRegressor
+
+model=LinearRegression()
+model.fit(x_train, y_train)
+
+rfe=RFE(model,15)
+rfe=rfe.fit(x_train, y_train)
+
+selected_features=list(zip(x_train.columns,rfe.support_,rfe.ranking_)) #checking the selected features
+selected_features
+
+index=x_train.columns[rfe.support_]
+x_train_new=x_train[index]
+x_train_new.head()
+
+def buildmodel(x,y):
+    x=sm.add_constant(x)
+    model=sm.OLS(y,x).fit()
+    print(model.summary())
+    return x
+
+#Running Regression Models
+
+#Model 1    
+model_1=buildmodel(x_train_new,y_train)
+
+x_train_new=x_train_new.drop(['dohcv'], axis=1)
+
+#Model 2
+model_2=buildmodel(x_train_new, y_train)
+
+x_train_new=x_train_new.drop(['two'],axis=1)
+
+#Model 3
+model_3=buildmodel(x_train_new, y_train)
+
+x_train_new=x_train_new.drop(['three'],axis=1)
+
+#Model 4
+model_4=buildmodel(x_train_new,y_train)
+
+x_train_new=x_train_new.drop(['hardtop'],axis=1)
+
+#This is the final model. Hence, it will be named as f_model.
+f_model=model_4
+
+#checking vif value
+
+def checkVIF(x):
+    vif = pd.DataFrame()
+    vif['Features'] = x.columns
+    vif['VIF'] = [variance_inflation_factor(x.values, i) for i in range(x.shape[1])]
+    vif['VIF'] = round(vif['VIF'], 2)
+    vif = vif.sort_values(by = "VIF", ascending = False)
+    return(vif)
+    
+checkVIF(model_4)    
+
+model_new=model_4.drop(['Low','Medium-Low','enginesize'], axis=1)
+model_5=buildmodel(model_new, y_train) #checking OLS Results
+checkVIF(model_5) #checking vif value
+
+model_new=model_new.drop(['boreratio','five','six'], axis=1)
+model_6=buildmodel(model_new,y_train)
+checkVIF(model_6)
+
+#VIF Value is under control. Now, this is our final regression model.
+
+final_rm=model_6
+
+#Now, to check errors, we will drop one feature, lets say hatchback.
+model_check=model_6.drop(['ohcv'], axis=1)
+model_check=buildmodel(model_check, y_train)
+checkVIF(model_check)
+
+#dist plot for residual analysis
+
+lm=sm.OLS(y_train,model_check).fit()
+y_train_price=lm.predict(model_check)
+
+fig = plt.figure()
+sns.distplot((y_train - y_train_price), bins = 20)
+fig.suptitle('Error Terms', fontsize = 20)                  # Plot heading 
+plt.xlabel('Errors', fontsize = 18)  
+
+
+#5. Prediction and Evaluation
+
+#selecting the highly correlated values
+df_test[num_vars] = scaler.fit_transform(df_test[num_vars])
+
+#splitting into x and y
+y_test=df_test.pop('price')
+x_test=df_test
+
+# Now let's use our model to make predictions.
+X_train_new = model_check.drop('const',axis=1)
+# Creating X_test_new dataframe by dropping variables from X_test
+X_test_new = x_test[X_train_new.columns]
+# Adding a constant variable 
+X_test_new = sm.add_constant(X_test_new)
+
+y_pred=lm.predict(X_test_new)
+y_test
+y_pred
+price=pd.concat([y_test,y_pred],axis=1)
+price=price.rename(columns={0:'pred_price'}) #price prediction using linear regression
+price=price.sort_index()
+price.plot()
+
+from sklearn.metrics import r2_score 
+acc=r2_score(y_test, y_pred)
+print('The Accuracy Score is : ',(acc*100).round(3),'%') #Accuracy Score with Linear Regression
+
+from sklearn.ensemble import RandomForestRegressor
+rf=RandomForestRegressor()
+rf.fit(x_train,y_train)
+
+rf_pred=pd.Series(rf.predict(x_test)) #price prediction using random forest 
+rf_pred
+acc_rf=r2_score(y_test, rf_pred)
+print('The Accuracy Score is : ',(acc_rf*100).round(3),'%') #Accuracy Score with Random Forest Regressor
+
+c= [i for i in range(1,83,1)] # generating index 
+fig = plt.figure(figsize=(15,5)) 
+plt.plot(c,y_test, color="blue", linewidth=2.5, linestyle="-") #Plotting Actual
+plt.plot(c,rf_pred, color="red",  linewidth=2.5, linestyle="-") #Plotting predicted
+fig.suptitle('Actual and Predicted', fontsize=20)              # Plot heading 
+plt.xlabel('Index', fontsize=18)                               # X-label
+plt.ylabel('Car Price', fontsize=16)                       # Y-label
+
+# Plotting y_test and y_pred to understand the spread.
+fig = plt.figure()
+plt.scatter(y_test,y_pred)
+fig.suptitle('y_test vs y_pred', fontsize=20)              # Plot heading 
+plt.xlabel('y_test', fontsize=18)                          # X-label
+plt.ylabel('y_pred', fontsize=16)                          # Y-label
+
+#plotting y_test and rf_pred to understand the spread.
+fig = plt.figure()
+plt.scatter(y_test,rf_pred)
+fig.suptitle('y_test vs rf_pred', fontsize=20)              # Plot heading 
+plt.xlabel('y_test', fontsize=18)                          # X-label
+plt.ylabel('rf_pred', fontsize=16)                          # Y-label
+
 
 
